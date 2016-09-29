e6cdaefc97c40aa0dd2721fd4258a2f788728b25
sandmaking <xuanxuankuaile@gmail.com>
2016-09-29 09:09:15 +0800

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,9 @@
+
+//document.writeln("<div style=\"width:160px;height:245px;float:right; z-index:999;position:fixed !important; top/**/:220px; position:absolute; top:expression(offsetParent.scrollTop+400);right:-40px;\"> ");
+/*
+document.writeln("<div id=\"lpButDivID-1331955599520\"></div>");
+document.writeln("<script language=\"javascript\" src=\"http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT21646611&float=0&lng=en\"></script>");
+document.writeln("<a href=\"javascript:void(0)\" onclick=openZoosUrl(\'chatwin\')  target=\"_self\"><img src=\"chat.gif\" border=\"0\"></a>");
+document.writeln("</div>");
+*/
+document.write('<script src="http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT21646611&float=1&lng=en"><\/script>');   
